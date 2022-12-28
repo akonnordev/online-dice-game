1319fcec230bbf1766b95e1602d67fdb14b80821
Akonnor Owusu Larbi <akonnor93@gmail.com>
2022-12-27 21:09:53 -0800

    first commit for developing an online dice game

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+let image = [
+  "images/dice 1.png",
+  "images/dice 2.png",
+  "images/dice 3.png",
+  "images/dice 4.png",
+  "images/dice 5.png",
+  "images/dice 6.png",
+];
+
+const firstdiceValue = Math.floor(Math.random() * 6);
+const SeconddiceValue = Math.floor(Math.random() * 6);
+document.querySelector(".roll-dice").addEventListener("click", function () {
+  document.querySelector("#img1").setAttribute("src", image[firstdiceValue]);
+  document.querySelector("#img2").setAttribute("src", image[SeconddiceValue]);
+  let total = firstdiceValue + 1 + (SeconddiceValue + 1);
+  document.querySelector(
+    ".score"
+  ).innerHTML = ` Your total score is now ${total}`;
+});
